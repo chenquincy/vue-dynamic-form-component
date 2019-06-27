c95b17ef36bdba97c48a74ccb63e17f44ec9bd4f
chenquincy <mail@quincychen.cn>
2019-06-27 20:04:25 +0800

    fix: export component error.

--- packages/index.js
+++ packages/index.js
@@ -1,24 +1,15 @@
 import DynamicForm from './dynamic-form/index'
 
-const components = [
-  DynamicForm
-]
-
 const install = function (Vue) {
   if (install.installed) {
     return false
   }
   install.installed = true
-  components.map(component => {
-    Vue.component(component.name, component)
-  })
+  DynamicForm.install(Vue)
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
 
-export default {
-  install,
-  DynamicForm
-}
+export default DynamicForm
